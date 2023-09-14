5ae5d78a357a116d824f054b7acc8772c67540a5
Maria Alfaro <139033669+malfba@users.noreply.github.com>
2023-09-13 18:32:06 -0600

    Update snake.py

--- snake.py
+++ snake.py
@@ -35,7 +35,7 @@ def move():
     head.move(aim)
 
     if not inside(head) or head in snake:
-        square(head.x, head.y, 9, 'red')
+        square(head.x, head.y, 9, 'blue')
         update()
         return
 
